87e01d4c536df0557805ca6b236a885a0dec1469
fpereira <fernando.pereira@poptrade.com.br>
2018-05-27 20:27:40 -0300

    Initial commit

---
+++ config/dbConnection.js
@@ -0,0 +1,21 @@
+//exportar o módulo mongodb
+var mongo = require("mongodb");
+
+var connMongoDB = function() {
+  console.log("entrou na função de conexão");
+  var db = new mongo.Db(
+    "got",
+    new mongo.Server(
+      "localhost", //string contendo o endereço do banco de dados
+      27017, //porta de conexão
+      {}
+    ),
+    {}
+  );
+
+  return db;
+}
+
+module.exports = function(){
+  return connMongoDB;
+}
